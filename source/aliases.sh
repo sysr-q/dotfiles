a1a05d6ccfb6a5bd7134e236f0458bc99453c6ec
Chris Carter <chris@gibsonsec.org>
2014-01-20 17:19:42 +0000

    Broken?

--- source/aliases.sh
+++ source/aliases.sh
@@ -4,12 +4,3 @@ alias ls="ls --color=auto"
 alias ll="ls -l"
 alias wtc="curl -s http://whatthecommit.com/index.txt"
 
-function make() {
-	if [[ "$@" == "me a sandwich" ]]; then
-		echo "What? Make it yourself!"
-		return 1
-	else
-		\make "$@"
-		return $?
-	fi
-}
