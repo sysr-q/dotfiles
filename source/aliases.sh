bda3a0ffb0052cbc3ddf4edec207a4e681867206
Chris Carter <chris@gibsonsec.org>
2014-01-20 00:02:07 +0000

    xkcd references [because edgy maymays]

--- source/aliases.sh
+++ source/aliases.sh
@@ -3,3 +3,13 @@ alias grep="grep --color=auto"
 alias ls="ls --color=auto"
 alias ll="ls -l"
 alias wtc="curl -s http://whatthecommit.com/index.txt"
+
+function make() {
+	if [[ "$@" == "me a sandwich" ]]; then
+		echo "What? Make it yourself!"
+		return 1
+	else
+		\make "$@"
+		return $?
+	fi
+}
