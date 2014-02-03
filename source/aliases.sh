7f459133a00f832cf3e34bcede8d233243ddbafc
Chris Carter <chris@gibsonsec.org>
2014-02-03 23:22:47 +1100

    sprunge()

--- source/aliases.sh
+++ source/aliases.sh
@@ -3,4 +3,9 @@ alias grep="grep --color=auto"
 alias ls="ls --color=auto"
 alias ll="ls -l"
 alias wtc="curl -s http://whatthecommit.com/index.txt"
-
+function sprunge() {
+	data=$(cat $*)
+	url=$(curl -F "sprunge=$data" http://sprunge.us 2>/dev/null)
+	echo $url
+	echo $url | xclip
+}
