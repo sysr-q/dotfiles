31641285d11821ddb1e1cd373ce161148e61dd54
Chris Carter <chris@gibsonsec.org>
2014-02-25 18:14:21 +1100

    Added expose-remote alias.

--- source/aliases.sh
+++ source/aliases.sh
@@ -3,9 +3,16 @@ alias grep="grep --color=auto"
 alias ls="ls --color=auto"
 alias ll="ls -l"
 alias wtc="curl -s http://whatthecommit.com/index.txt"
+
 function sprunge() {
 	data=$(cat $*)
 	url=$(curl -F "sprunge=$data" http://sprunge.us 2>/dev/null)
 	echo $url
 	echo $url | xclip
 }
+
+function expose-remote() {
+	# expose-remote <host> <remote-port> <local-port>
+	ssh -N -R $2:localhost:$3 $1
+}
+
