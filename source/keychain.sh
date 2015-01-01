a09c39a06ec3bb4cafedf93b4e5c239602c03be0
Chris Carter <chris@gibsonsec.org>
2015-01-02 04:54:44 +1100

    Try to add id_ecdsa to keychain as well.

--- source/keychain.sh
+++ source/keychain.sh
@@ -1,4 +1,6 @@
+KEYS=(id_rsa id_ecdsa)
+
 if command keychain >/dev/null 2>&1; then
 	# Let's keychain it up in this fucker.
-	eval $(keychain --eval --agents ssh -Q --quiet ~/.ssh/id_rsa)
+	eval $(keychain --eval --agents ssh -Q --quiet $KEYS)
 fi
